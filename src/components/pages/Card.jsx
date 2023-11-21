d6c18a9f1e2d529f097b2ab5c4aef9e59e29a4f7
Dinesh Suthar <suthardinesh626@gmail.com>
2023-11-21 22:14:24 +0530

    api is upated, filter are working

--- src/components/pages/Card.jsx
+++ src/components/pages/Card.jsx
@@ -7,12 +7,15 @@ const Card = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [currentPage, setCurrentPage] = useState(1);
+  const [checkedBrands, setCheckedBrands] = useState([]);
+  const [checkedCategories, setCheckedCategories] = useState([]);
   const pageSize = 9;
 
   useEffect(() => {
     setLoading(true);
     fetch(
-      `https://dummyjson.com/products?skip=${(currentPage - 1) * pageSize
+      `https://dummyjson.com/products?skip=${
+        (currentPage - 1) * pageSize
       }&limit=${pageSize}`
     )
       .then((response) => response.json())
@@ -48,39 +51,76 @@ const Card = () => {
             <p>Loading data...</p>
           ) : (
             <div className=" flex flex-wrap justify-center gap-2   ">
-              {products.map((product) => (
-                <div className="flex flex-row border-4 bg-red-200 w-64 m-5 p-5 rounded-2xl shadow-2xl cursor-pointer">
-                  <div
-                    key={product.id}
-                    className="flex flex-col justify-between items-center gap-1"
-                  >
-                    <img
-                      className="rounded-2xl my-4 h-48 w-40"
-                      src="https://cdn1.smartprix.com/rx-ibsJ2hNrF-w1200-h1200/bsJ2hNrF.jpg"
-                      alt=""
-                    />
-                    <hr className="" />
-                    <h2 className=" font-bold ">{product.title}</h2>
-                    <p className="font-semibold text-justify">
-                      {product.description}
-                    </p>
-                    <p className="font-medium ">Price: ${product.price}</p>
-                    <p className="font-medium">Rating: {product.rating}</p>
-                    <p className="font-medium">Stock: {product.stock} units</p>
-                    <div className="flex felx-row gap-4 ">
-                      <button className="flex flex-row justify-center items-center text-slate-200 font-semibold text-sm border-4  rounded-lg p-2 bg-slate-500 ">
+              {products
+                .filter(
+                  (product) =>
+                    (checkedBrands.length === 0 ||
+                      checkedBrands.includes(product.brand)) &&
+                    (checkedCategories.length === 0 ||
+                      checkedCategories.includes(product.category))
+                )
+                .map((product) => (
+                  <div className="flex flex-row border-4 bg-red-200 w-64 m-5 p-5 rounded-2xl shadow-2xl cursor-pointer">
+                    <div
+                      key={product.id}
+                      className="flex flex-col justify-between items-center gap-1"
+                    >
+                      <div>
+                        <img src={product.thumbnail} alt="" />
+                      </div>
+                      <hr className="" />
+                      <h2 className=" font-bold ">{product.title}</h2>
+                      <p className="font-semibold text-justify">
+                        {product.description}
+                      </p>
+                      <p className="font-medium ">Price: ${product.price}</p>
+                      <p className="font-medium">Rating: {product.rating}</p>
+                      <p className="font-medium">
+                        Stock: {product.stock} units
+                      </p>
+                      <div className="flex felx-row gap-4 ">
+                        <button className="flex flex-row justify-center items-center text-slate-200 font-semibold text-sm border-4  rounded-lg p-2 bg-slate-500 ">
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="20"
+                            height="20"
+                            id="add"
+                          >
+                            <g fill="#000100">
+                              <rect
+                                width="4"
+                                height="20"
+                                x="8"
+                                rx="1.98"
+                              ></rect>
+                              <rect
+                                width="20"
+                                height="4"
+                                y="8"
+                                rx="1.98"
+                              ></rect>
+                            </g>
+                          </svg>
 
-                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" id="add"><g fill="#000100"><rect width="4" height="20" x="8" rx="1.98"></rect><rect width="20" height="4" y="8" rx="1.98"></rect></g></svg>
-
-                        <svg className="h-8 w-8" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64" id="cart"><g fill="#134563"><path d="M48.5 45.7H18.2c-.5 0-.9-.2-1.1-.6-.3-.4-.3-.9-.1-1.3l2.6-6.6L17 12.6H8.6V9.8h9.6c.7 0 1.3.5 1.4 1.2l2.8 26.1c0 .2 0 .4-.1.7l-2 5h28.2v2.9"></path><path d="m21.3 38.8-.6-2.7 31.9-6.6V18.2h-33v-2.8H54c.8 0 1.4.6 1.4 1.4v13.8c0 .7-.5 1.2-1.1 1.3l-33 6.9M49.9 54c-3 0-5.5-2.5-5.5-5.5s2.5-5.5 5.5-5.5 5.5 2.5 5.5 5.5-2.5 5.5-5.5 5.5zm0-8.3c-1.5 0-2.8 1.2-2.8 2.8s1.2 2.8 2.8 2.8 2.8-1.2 2.8-2.8-1.3-2.8-2.8-2.8zm-33 8.3c-3 0-5.5-2.5-5.5-5.5s2.5-5.5 5.5-5.5 5.5 2.5 5.5 5.5-2.5 5.5-5.5 5.5zm0-8.3c-1.5 0-2.8 1.2-2.8 2.8s1.2 2.8 2.8 2.8 2.8-1.2 2.8-2.8-1.3-2.8-2.8-2.8z"></path></g></svg>
-                      </button>
-                      <button className="text-slate-200 font-semibold text-lg border-4 rounded-lg p-2 bg-slate-500">
-                        Buy now
-                      </button>
+                          <svg
+                            className="h-8 w-8"
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 64 64"
+                            id="cart"
+                          >
+                            <g fill="#134563">
+                              <path d="M48.5 45.7H18.2c-.5 0-.9-.2-1.1-.6-.3-.4-.3-.9-.1-1.3l2.6-6.6L17 12.6H8.6V9.8h9.6c.7 0 1.3.5 1.4 1.2l2.8 26.1c0 .2 0 .4-.1.7l-2 5h28.2v2.9"></path>
+                              <path d="m21.3 38.8-.6-2.7 31.9-6.6V18.2h-33v-2.8H54c.8 0 1.4.6 1.4 1.4v13.8c0 .7-.5 1.2-1.1 1.3l-33 6.9M49.9 54c-3 0-5.5-2.5-5.5-5.5s2.5-5.5 5.5-5.5 5.5 2.5 5.5 5.5-2.5 5.5-5.5 5.5zm0-8.3c-1.5 0-2.8 1.2-2.8 2.8s1.2 2.8 2.8 2.8 2.8-1.2 2.8-2.8-1.3-2.8-2.8-2.8zm-33 8.3c-3 0-5.5-2.5-5.5-5.5s2.5-5.5 5.5-5.5 5.5 2.5 5.5 5.5-2.5 5.5-5.5 5.5zm0-8.3c-1.5 0-2.8 1.2-2.8 2.8s1.2 2.8 2.8 2.8 2.8-1.2 2.8-2.8-1.3-2.8-2.8-2.8z"></path>
+                            </g>
+                          </svg>
+                        </button>
+                        <button className="text-slate-200 font-semibold text-lg border-4 rounded-lg p-2 bg-slate-500">
+                          Buy now
+                        </button>
+                      </div>
                     </div>
                   </div>
-                </div>
-              ))}
+                ))}
             </div>
           )}
         </div>
@@ -97,11 +137,14 @@ const Card = () => {
           >
             <GrNext />
           </button>
-
         </div>
       </div>
       <div className="bg-red-300  min-w-1/6">
-        <Filter products={products} />
+        <Filter
+          products={products}
+          setCheckedBrands={setCheckedBrands}
+          setCheckedCategories={setCheckedCategories}
+        />
       </div>
     </div>
   );
--- src/components/pages/Filter.jsx
+++ src/components/pages/Filter.jsx
@@ -1,67 +1,109 @@
-const Filter = ({ products }) => {
-    console.log(products);
 
-    return (
-        <div className="w-full">
-            <div className="flex flex-row p-4">
-                <p className="text-lg font-semibold px-3">Filter</p>
-                <svg
-                    className="cursor-pointer"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="22"
-                    height="20"
-                    viewBox="0 0 22 20"
-                    id="filter"
-                >
-                    <g
-                        fill="none"
-                        fill-rule="evenodd"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                    >
-                        <g
-                            stroke="#000"
-                            stroke-width="2"
-                            transform="translate(-1614 -1629)"
-                        >
-                            <g transform="translate(1615 1630)">
-                                <path d="M20 0H0l8 9.46V16l4 2V9.46z"></path>
-                            </g>
-                        </g>
-                    </g>
-                </svg>
-            </div>
-            <div className="flex flex-col items-center">
-                <hr className=" w-1/2 h-3" />
-            </div>
 
-            <div className="flex felx-row gap-9">
-                <div className="flex flex-col p-3">
-                    <h3 className="text-sm underline font-bold text-yellow-200">BRAND</h3>
-                    {products?.map((product) => (
-                        <div className="flex flex-col cursor-pointer">
-                            <div key={product.id}>
-                                <input type="checkbox" /> {product.brand}
-                            </div>
-                        </div>
-                    ))}
-                </div>
+const Filter = ({ products, setCheckedBrands, setCheckedCategories }) => {
+  const uniqueBrands = products.reduce((unique, product) => {
+    if (!unique.some((item) => item.brand === product.brand)) {
+      unique.push(product);
+    }
+    return unique;
+  }, []);
 
-                <div className="flex flex-col p-3">
-                    <h3 className="text-sm underline font-bold text-yellow-200">
-                        CATEGORY
-                    </h3>
-                    {products?.map((product) => (
-                        <div className="flex flex-col cursor-pointer">
-                            <div key={product.id}>
-                                <input type="checkbox" /> {product.category}
-                            </div>
-                        </div>
-                    ))}
-                </div>
+  const uniqueCategory = products.reduce((unique, product) => {
+    if (!unique.some((item) => item.category === product.category)) {
+      unique.push(product);
+    }
+    return unique;
+  }, []);
+
+  const handleBrandCheck = (brand) => {
+    setCheckedBrands((prev) => {
+      if (prev.includes(brand)) {
+        return prev.filter((b) => b !== brand);
+      } else {
+        return [...prev, brand];
+      }
+    });
+  };
+
+  const handleCategoryCheck = (category) => {
+    setCheckedCategories((prev) => {
+      if (prev.includes(category)) {
+        return prev.filter((c) => c !== category);
+      } else {
+        return [...prev, category];
+      }
+    });
+  };
+
+  return (
+    <div className="w-full">
+      <div className="flex flex-row p-4">
+        <p className="text-lg font-semibold px-3">Filter</p>
+        <svg
+          className="cursor-pointer"
+          xmlns="http://www.w3.org/2000/svg"
+          width="22"
+          height="20"
+          viewBox="0 0 22 20"
+          id="filter"
+        >
+          <g
+            fill="none"
+            fill-rule="evenodd"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <g
+              stroke="#000"
+              stroke-width="2"
+              transform="translate(-1614 -1629)"
+            >
+              <g transform="translate(1615 1630)">
+                <path d="M20 0H0l8 9.46V16l4 2V9.46z"></path>
+              </g>
+            </g>
+          </g>
+        </svg>
+      </div>
+      <div className="flex flex-col items-center">
+        <hr className=" w-1/2 h-3" />
+      </div>
+
+      <div className="flex felx-row gap-9">
+        <div className="flex flex-col p-3">
+          <h3 className="text-sm underline font-bold text-yellow-200">BRAND</h3>
+          {uniqueBrands?.map((product) => (
+            <div className="flex flex-col cursor-pointer">
+              <div key={product.id}>
+                <input
+                  type="checkbox"
+                  onChange={() => handleBrandCheck(product.brand)}
+                />{" "}
+                {product.brand}
+              </div>
             </div>
+          ))}
         </div>
-    );
+        <div className="flex flex-col p-3">
+          <h3 className="text-sm underline font-bold text-yellow-200">
+            CATEGORY
+          </h3>
+          {uniqueCategory?.map((product) => (
+            <div className="flex flex-col cursor-pointer">
+              <div key={product.id}>
+                <input
+                  type="checkbox"
+                  onChange={() => handleCategoryCheck(product.category)}
+                />
+                <label htmlFor=""> {product.category} </label>
+              </div>
+            </div>
+          ))}
+        </div>
+        -
+      </div>
+    </div>
+  );
 };
 
 export default Filter;
