caf791db09609773fe8521214f571f75acf53eb9
Dinesh Suthar <suthardinesh626@gmail.com>
2023-12-16 20:29:49 +0530

    carousal is fixed

--- src/components/pages/HomeCarousel.jsx
+++ src/components/pages/HomeCarousel.jsx
@@ -8,7 +8,7 @@ const HomeCarousel = () => {
 
     return (
         <div>
-            <div className="min-h-1/2 sm:h-64 md:h-80 lg:h-96 xl:h-120 2xl:h-144">
+            <div className="hidden sm:block min-h-1/2 sm:h-64 md:h-80 lg:h-96 xl:h-120 2xl:h-144">
                 <Carousel slide={true}>
                     {products.map((item, index) => (
                         <img className='w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 2xl:w-1/6' key={index} src={item.thumbnail} alt={item.name} />
